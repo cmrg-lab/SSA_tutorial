82982c35b6c308ef9160889f53cff016bf1be1ae
Anna Qi <annaqi13@gmail.com>
2025-06-18 12:52:24 -0700

    remove unused biv-me-main directory

--- biv-me-main/bivme/__init__.py
+++
@@ -1,4 +0,0 @@
-from os.path import dirname
-from pathlib import Path
-MODEL_RESOURCE_DIR = Path(dirname(__file__)) / '../model'
-TEST_RESOURCE_DIR = Path(dirname(__file__)) / '../../tests/test_data'
